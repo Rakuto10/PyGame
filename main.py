8de5eed9480daf1b617af43f4a98db0a0e3002d9
Rakuto10 <161914609+Rakuto10@users.noreply.github.com>
2024-03-30 11:46:15 +0500

    Красноярск

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pygame as pg
 
-    class game:
-        def __init__(self) -> object:
-            pass
+pg.init()
+
+class Game:
+    def __init__(self) -> object:
+        windows_size: tuple = (300,000)
+        pg.display.set_caption("Моя игра моя игра она же как и я")
+        self.screen = pg.display.set_mode(windows_size)
+        background_color = (0, 0, 255)  # синий
+
+
+        self.screen.fill(background_color)
+
+        pg.display.flip()
+
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    exit()
+
+a = Game()
